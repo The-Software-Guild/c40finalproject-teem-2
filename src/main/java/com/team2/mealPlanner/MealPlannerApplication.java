c8c4d4d7e0bd6ce7711cbc4ca29554816b92acdd
Ha Nguyen <nguyenthingocha2@gmail.com>
2021-04-01 11:28:09 -0400

    second commits

---
+++ src/main/java/com/team2/mealPlanner/MealPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.team2.mealPlanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealPlannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team2/mealPlanner/MealPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team2.mealPlanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
