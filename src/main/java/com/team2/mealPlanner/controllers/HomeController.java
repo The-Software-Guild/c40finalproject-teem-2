2d321ba7ef8f8eefdb1cf2b0f9e7733800dd316a
medrist <mboudouar@gmail.com>
2021-04-06 12:47:37 -0400

    added service

--- src/main/java/com/team2/mealPlanner/controllers/HomeController.java
+++ src/main/java/com/team2/mealPlanner/controllers/HomeController.java
@@ -2,7 +2,9 @@ package com.team2.mealPlanner.controllers;
 
 
 import com.team2.mealPlanner.entities.User;
+import com.team2.mealPlanner.services.MealService;
 import com.team2.mealPlanner.services.UserServiceImp;
+import com.team2.mealPlanner.utils.ApiMeal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.List;
 import java.util.logging.Logger;
 
 @Controller
@@ -19,6 +22,10 @@ public class HomeController {
 
     @Autowired
     private UserServiceImp userServiceImp;
+
+    @Autowired
+    private MealService mealService;
+
 
     @GetMapping("/")
     public String root() {
@@ -39,6 +46,13 @@ public class HomeController {
     public String signup() {
 
         return "signup";
+    }
+
+    @GetMapping("home")
+    public String home(Model model) {
+        List<ApiMeal> meals = mealService.getMealsFromApi();
+        model.addAttribute("meals", meals);
+        return "homePage";
     }
 
     @RequestMapping(value = "/signup", method = RequestMethod.POST)
